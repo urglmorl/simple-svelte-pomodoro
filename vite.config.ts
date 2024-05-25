20a4fe735b34ed452cf6dfe13937b772b9c6ac0c
Zolotov Nikita <n.zolotov@atomyze.team>
2024-05-25 11:59:00 +0300

    deployment staff

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(() => {
+	return {
+		plugins: [
+			sveltekit()
+		],
+		build: {
+			target: 'esnext',
+			sourcemap: true,
+		}
+	};
 });
